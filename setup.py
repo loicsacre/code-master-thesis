2a689cbbcdb54cdee72bfc7e0fbdfe4bf9bbcb82
loicsacre <32166486+loicsacre@users.noreply.github.com>
2019-10-22 11:58:40 +0200

    Adding missing setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='loicsacre-master-thesis',
+    version='1.0.0',
+    description='Code for the Loic Sacre\'s Master Thesis',
+    packages=find_packages(),
+    url='https://uliege.cytomine.org',
+    license='LICENSE',
+)
